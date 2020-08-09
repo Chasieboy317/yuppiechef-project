be9392a3c6f7f2d25991a6e6c929d4f838db68fe
Chase Abrahams <cabrahams317@gmail.com>
2020-08-09 03:29:36 +0200

    fixed update method

--- web/index.php
+++ web/index.php
@@ -82,7 +82,7 @@ $app->get('/get_review/{id}', function($id) use($app) {
 });
 
 $app->post('/edit_review', function() use($app) {
-  $app['db']->update('reviews', array('description' => $_POST['description']));
+  $app['db']->update('reviews', array('description' => $_POST['description']), array('id' => $_POST['id']));
   return new Response('Successfully updated review', 200);
 });
 
