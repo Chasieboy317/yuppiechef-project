5a1a6b927e54086b69daa699f00b89d64449df52
Chase Abrahams <cabrahams317@gmail.com>
2020-08-10 17:30:03 +0200

    added debug statements

--- web/index.php
+++ web/index.php
@@ -127,6 +127,8 @@ $app->get('/report/get_data', function() use($app) {
 
 //return the depending on the error
 $app->error(function (\Exception $e, Request $request, $code) use($app) {
+  $app['monolog']->addDebug("exception is $e");
+  $app['monolog']->addDebug("code is $code");
   switch ($code) {
     case 404:
       return $app['twig']->render('notfound.twig');
